1b5270cdcb8d391cf811c40086f91011fc8a4371
Hansel Sequeira <sequeirahansel@gmail.com>
2023-10-29 12:47:51 +0530

    Added filtering logic, used useState() hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,22 @@
-import { gridElements } from "../../utils/mockData";
+import { useState } from "react";
 import Card from "./Card";
+import { gridElements } from "../../utils/mockData";
 
 const Body = () => {
+
+    const [restaurants, setElements] = useState(gridElements.restaurants);
+
     return (
         <div className="body-div">
-            <div className="search-bar">
-                Search Bar
+            <div className="filter-btn">
+                <button onClick={() => {
+                    setElements(restaurants.filter(restaurant => restaurant.info.avgRating > 4.0));
+                }}>
+                    Filter Top Restaurants
+                </button>
             </div>
             <div className="card-container">
-                {gridElements.restaurants.map(restaurant => <Card key={restaurant.info.id} element={restaurant.info} />)}
+                {restaurants.map(restaurant => <Card key={restaurant.info.id} element={restaurant.info} />)}
             </div>
         </div>
     )
